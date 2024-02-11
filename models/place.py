a6e59de57406b1e4b8171768f1b08b23d00a3792
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-02-11 17:52:12 +0300

    Place

--- models/place.py
+++ models/place.py
@@ -2,7 +2,6 @@
 """Defines place with attributes helpful in location of the airbnb"""
 
 from models.base_model import BaseModel
-from typing import List
 
 
 class Place(BaseModel):
@@ -17,4 +16,4 @@ class Place(BaseModel):
     price_by_night: int = 0
     latitude: float = 0.0
     longitude: float = 0.0
-    amenity_ids: List[str] = []
+    amenity_ids: list[str] = []
