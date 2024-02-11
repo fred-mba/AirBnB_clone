48f6f3efe738d2c3e055c75a95006abe56eb0640
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-02-11 12:11:33 +0300

    city

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """Defines city attributes -- state_id and name"""
 from models.base_model import BaseModel
 
+
 class City(BaseModel):
     """Class representation of city"""
     state_id: str = ''
