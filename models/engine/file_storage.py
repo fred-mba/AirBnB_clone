8249e63738a2b677e59290e89f307f45d1fed3ff
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-02-11 22:29:37 +0300

    File storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -28,7 +28,7 @@ class FileStorage:
         """Serializes __objects to JSON file, that is,
         covert objects into a format that can easily be stored"""
         serialized_obj = {}
-        for key, obj in self.__objects.items():
+        for key, obj in self.all().items():
             serialized_obj[key] = obj.to_dict()
 
         with open(self.__file_path, 'w') as file:
@@ -38,16 +38,28 @@ class FileStorage:
         """Deserializes JSON file to __objects, that is,
         converting data back into its origial data structure or object
         allowing it to be used in the program as it was before."""
+
+        class_list = {
+            'Amenity': Amenity,
+            'BaseModel': BaseModel,
+            'City': City,
+            'Place': Place,
+            'Review': Review,
+            'State': State,
+            'User': User,
+        }
+
         try:
-            with open(self.__file_path, 'r') as file:
+            with open(self.__file_path, 'r', encoding="UTF-8") as file:
                 data = json.load(file)
+
             for key, value in data.items():
                 """Tuple unpacking"""
-                class_name, obj_id = key.split('.')
-                class_obj = globals()[class_name]
+                class_name = value['__class__']
+                class_obj = class_list[class_name]
                 obj = class_obj(**value)
-                setattr(obj, '__class__', class_obj)
-                self.__objects[key] = obj
+                all_objects = self.all()
+                all_objects[key] = obj
 
         except FileNotFoundError:
             pass
