b9a39feea1528655a5114a3e81f9abc134e27cb6
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-02-13 22:10:26 +0300

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ Define common attributes/methods for other classes
 """
 from datetime import datetime
 import uuid
-import models
+from models import storage
 
 
 class BaseModel:
@@ -27,7 +27,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         """prints: [<class name>] (<self.id>) <self.__dict__>"""
@@ -37,7 +37,7 @@ class BaseModel:
     def save(self):
         """ updates the public instance attribute updated_at"""
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values"""
