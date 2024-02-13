6d4095a00de3df6592ffa8a7ccb33e4d4f31b6dd
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-02-13 07:20:49 +0300

    The console

--- console.py
+++ console.py
@@ -15,7 +15,6 @@ from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.__init__ import storage
 from models.city import City
-from models.engine.file_storage import FileStorage
 from models.place import Place
 from models.review import Review
 from models.state import State
